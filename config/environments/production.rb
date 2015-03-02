f236a61939271dddcb0a803e034d52f238cb2d9c
tommybyrne <tbyrn687@gmail.com>
2015-03-01 21:08:14 -0600

    Initial Setup of Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # Mail setup for production
+    config.action_mailer.default_url_options = { host: 'hometownadventure.com' }
 end
