30c9b927413f7d9605432458119bed020538a80f
tommybyrne <tbyrn687@gmail.com>
2015-03-11 22:08:37 -0500

    Testing background image in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -78,5 +78,5 @@ Rails.application.configure do
 
 
   # Mail setup for production
-    config.action_mailer.default_url_options = { host: 'hometownadventure.com' }
+  config.action_mailer.default_url_options = { host: 'hometownadventure.com' }
 end
